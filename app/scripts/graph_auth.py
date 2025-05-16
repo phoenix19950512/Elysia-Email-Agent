425c5b75e279e75e2dea65d56d31ea2d82765b0d
phoenix19950512 <phoenix19950512@gmail.com>
2025-05-16 11:21:58 +0300

    Refactor ChromeDriver initialization in graph_auth.py by removing the service parameter and simplifying driver setup while retaining necessary options for headless operation.

--- app/scripts/graph_auth.py
+++ app/scripts/graph_auth.py
@@ -2,11 +2,9 @@ import argparse
 import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 
 def main():
     parser = argparse.ArgumentParser(
@@ -35,23 +33,15 @@ def main():
     email = args.email
     password = args.password
 
-    print('installing service')
-    driver_manager = ChromeDriverManager()
-    service = Service(driver_manager.install())
-    driver_path = driver_manager._get_driver_binary_path(driver_manager.driver)
-
-    print('initialize driver')
     options = Options()
-    options.binary_location = driver_path
-    options.add_argument("--no-sandbox")
     options.add_argument("--headless")
     options.add_argument("--window-size=1920,1080")
+    options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
     options.add_argument("--log-level=3")
     options.add_argument('--no-proxy-server')
-    options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options)
     driver.switch_to.window(driver.window_handles[0])
     wait = WebDriverWait(driver, 60)
     driver.get(url)
