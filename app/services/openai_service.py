031b5dc8e5ce334aa9bcf8f595371d0295828686
phoenix19950512 <phoenix19950512@gmail.com>
2025-05-16 19:02:18 +0300

    Clarify email template instructions in OpenAIService to specify that default folders of Microsoft Outlook should not be selected for email transfers.

--- app/services/openai_service.py
+++ app/services/openai_service.py
@@ -86,7 +86,7 @@ class OpenAIService:
         return f"""
         You need to customize the following email template to create a personalized reply
         to the email content below. Maintain a professional tone.
-        Don't select Inbox as all emails are transfered from there.
+        Don't select default folders of Microsoft Outlook.
         Don't include single or double quota in the result.
         
         Template name: {template.get("name", "(No Name)")}
