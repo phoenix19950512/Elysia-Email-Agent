13478b0dde8e023fe0822d45f6a03d68c28170f9
phoenix19950512 <phoenix19950512@gmail.com>
2025-05-16 10:43:18 +0300

    Enable headless mode in graph_auth.py for improved automation

--- app/scripts/graph_auth.py
+++ app/scripts/graph_auth.py
@@ -36,7 +36,7 @@ def main():
     password = args.password
 
     options = Options()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
